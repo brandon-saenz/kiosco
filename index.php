720f03cdaffd1749116232223a798d3829934a9f
brandon-saenz <programador@grupovalcas.mx>
2022-09-23 16:43:16 -0700

    test

--- index.php
+++ index.php
@@ -70,13 +70,13 @@ define('STASIS', $config['base_url']);
         
 
         <!-- FRAMWORK MATERIALIZE CSS -->
-        <link href="https://saevalcas.mx/pantallas/css/materialize.css" rel="stylesheet" type="text/css" />
-        <script src="https://saevalcas.mx/pantallas/js/materialize.min.js" type="text/javascript"></script>
+        <link href="css/materialize.css" rel="stylesheet" type="text/css" />
+        <script src="js/materialize.min.js" type="text/javascript"></script>
         
         <!-- ESTILOS Y FUNCIONES CUSTOM -->
 
-        <link href="<?php echo STASIS; ?>/css/styles.css" rel="stylesheet" type="text/css" />
-        <script src="<?php echo STASIS; ?>/js/scripts.js" type="text/javascript"></script>
+        <link href="/css/styles.css" rel="stylesheet" type="text/css" />
+        <script src="/js/scripts.js" type="text/javascript"></script>
 
         <!-- ASSETS -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
@@ -84,7 +84,7 @@ define('STASIS', $config['base_url']);
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
         
-        <link href="assets/logo.ico" rel="icon" type="image/x-icon" />
+        <link href="/assets/logo.ico" rel="icon" type="image/x-icon" />
 
     </head>
 
@@ -136,7 +136,7 @@ define('STASIS', $config['base_url']);
                                         </span>
                                         <div class="div-center m-v-05">
                                             <div class="qr-rancho" onclick="go_page(2)">
-                                                <img class="qr-img pointer" src="<?php echo STASIS; ?>/assets/qr-codes/solicitud_servicio.svg">
+                                                <img class="qr-img pointer" src="assets/qr-codes/solicitud_servicio.svg">
                                             </div>
                                         </div>
                                     </p>
