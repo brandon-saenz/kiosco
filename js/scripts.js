5d49c3d8b6d47ecdf33f521c4ccf9ba800872a5e
brandon-saenz <programador@grupovalcas.mx>
2022-10-01 11:38:51 -0700

    Correccion final

--- js/scripts.js
+++ js/scripts.js
@@ -39,11 +39,11 @@ $( document ).ready(function() {
 
         on('slideChange', () => {
             if (!swiper.params.debugger) return;
-            if(swiper.activeIndex==5){
+            if(swiper.activeIndex==4){
                 change_cobroplan();
-            }else if(swiper.activeIndex!=5){
+            }else if(swiper.activeIndex!=4){
                 change_rancho();
-            }else if(swiper.activeIndex<5){
+            }else if(swiper.activeIndex<4){
                 change_rancho();
             }else{
                 change_rancho();
